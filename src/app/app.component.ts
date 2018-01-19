517027066ac5d050c27b3b452c411f4a8bc5a001
AILLERON\voma <voma@ailleron.com>
2018-01-19 15:53:44 +0100

    lesson 28

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,25 @@ import { setTimeout } from 'timers';
 
 interface Files {
   name: string,
-  size: number,
+  size: any,
   type: string
 }
+
+import { FileSizePipe } from './filesize.pipe';
 
 @Component({
   selector: 'app-root',
   changeDetection: ChangeDetectionStrategy.Default,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers:[
+    FileSizePipe
+  ]
 })
 export class AppComponent implements OnInit{
 
   files: Files[];
+  mapped: Files[];
 
   ngOnInit() {
     this.files = [
@@ -23,7 +29,19 @@ export class AppComponent implements OnInit{
       {name: 'banner.jpg', size: 46477, type: 'image/jpg'},
       {name: 'background.png', size: 575476, type: 'image/png'}
     ];
+
+    this.mapped = this.files.map(file => {
+      return {
+        name: file.name,
+        type: file.type,
+        size: this.fileSizePipe.transform(file.size, 'mb')
+      };
+    });
   }
+
+  constructor(
+    private fileSizePipe: FileSizePipe
+  ) {}
 
 
 //   user: any = {
